5b0bdbc536981c0206196c92ab73daadc8de70c0
Dnyandeo Bansode <bansodednyanadeo@gmail.com>
2022-09-21 20:26:02 +0530

    Added-> Todisplay BridgeLAbz log using Propety Binding Tchnique.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld-Angular-App';
+  imgUrl = "../assets/Bl_Logo.png"
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz."
